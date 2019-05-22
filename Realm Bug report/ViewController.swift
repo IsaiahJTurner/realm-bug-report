d174a9adcf12ef174fadcc0970a12386a5c79a8b
Isaiah J Turner <3732588+IsaiahJTurner@users.noreply.github.com>
2019-05-22 18:39:46 -0400

    Fixed

--- Realm Bug report/ViewController.swift
+++ Realm Bug report/ViewController.swift
@@ -25,6 +25,49 @@ class DataStore {
     }
     let realm = try! Realm(configuration:  Realm.Configuration(fileURL: nil, inMemoryIdentifier: "Realm", syncConfiguration: nil, encryptionKey: nil, readOnly: false, schemaVersion: 0, migrationBlock: nil, deleteRealmIfMigrationNeeded: true, shouldCompactOnLaunch: nil, objectTypes: nil))
 }
+
+/// Used to group change notifications that occur within the same table view.
+class SectionedNotificationTokenBlock {
+    struct Changes {
+        var deletions = [IndexPath]()
+        var insertions = [IndexPath]()
+        var modifications = [IndexPath]()
+    }
+    private var queuedChanges = Changes()
+    /// Called with all queued changes.
+    private var updateBlock: (Changes) -> Void
+    /// Groups change notifications that occur within the same table view.
+    ///
+    /// - Parameter sectionedUpdateBlock: The block to be called whenever an update occurs.
+    init(_ updateBlock: @escaping (Changes) -> Void) {
+        self.updateBlock = updateBlock
+    }
+    /// Returns a closure you can provide as the handler for `observe`.
+    ///
+    /// - Parameter section: The section of the content in your table view.
+    func block<CollectionType>(forSection section: Int, initialBlock: (() -> Void)? = nil, errorBlock: ((Error) -> Void)? = nil) -> ((RealmCollectionChange<CollectionType>) -> Void) {
+        return { change in
+            switch change {
+            case .error(let error):
+                errorBlock?(error)
+            case .update(_, let deletions, let insertions, let modifications):
+                self.queuedChanges.deletions.append(contentsOf: deletions.map { IndexPath(row: $0, section: section) })
+                self.queuedChanges.insertions.append(contentsOf: insertions.map { IndexPath(row: $0, section: section) })
+                self.queuedChanges.modifications.append(contentsOf: modifications.map { IndexPath(row: $0, section: section) })
+                OperationQueue.current?.underlyingQueue?.async(execute: self.drainQueue)
+            case .initial(_):
+                initialBlock?()
+            }
+        }
+    }
+    /// Calls the sectioned notification token block with all changes.
+    private func drainQueue() {
+        guard queuedChanges.deletions.count > 0 || queuedChanges.insertions.count > 0 || queuedChanges.modifications.count > 0 else { return }
+        self.updateBlock(queuedChanges)
+        self.queuedChanges = Changes()
+    }
+}
+
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet var tableView: UITableView!
     let rows1 = DataStore.shared.realm.objects(Row.self).filter(NSPredicate(format: "title != %@", "section 2"))
@@ -37,34 +80,23 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        rows1NotificationToken = rows1.observe { (change) in
-            switch change {
-            case .error(let error):
-                print("Realm error reloading rows", error)
-            case .initial(_):
-                self.tableView.reloadSections([0], with: .automatic)
-            case .update(_, deletions: let deletions, insertions: let insertions, modifications: let modifications):
-                self.tableView.performBatchUpdates({
-                    self.tableView.insertRows(at: insertions.map { IndexPath(row: $0, section: 0) }, with: .automatic)
-                    self.tableView.reloadRows(at: modifications.map { IndexPath(row: $0, section: 0) }, with: .automatic)
-                    self.tableView.deleteRows(at: deletions.map { IndexPath(row: $0, section: 0) }, with: .automatic)
-                }, completion: nil)
-            }
+        let sectionedNotificationTokenBlock = SectionedNotificationTokenBlock { (changes) in
+            self.tableView.performBatchUpdates({
+                self.tableView.insertRows(at: changes.insertions, with: .automatic)
+                self.tableView.reloadRows(at: changes.modifications, with: .automatic)
+                self.tableView.deleteRows(at: changes.deletions, with: .automatic)
+            })
         }
-        rows2NotificationToken = rows2.observe { (change) in
-            switch change {
-            case .error(let error):
-                print("Realm error reloading rows", error)
-            case .initial(_):
-                self.tableView.reloadSections([1], with: .automatic)
-            case .update(_, deletions: let deletions, insertions: let insertions, modifications: let modifications):
-                self.tableView.performBatchUpdates({
-                    self.tableView.insertRows(at: insertions.map { IndexPath(row: $0, section: 1) }, with: .automatic)
-                    self.tableView.reloadRows(at: modifications.map { IndexPath(row: $0, section: 1) }, with: .automatic)
-                    self.tableView.deleteRows(at: deletions.map { IndexPath(row: $0, section: 1)}, with: .automatic)
-                }, completion: nil)
-            }
-        }
+        rows1NotificationToken = rows1.observe(sectionedNotificationTokenBlock.block(forSection: 0, initialBlock: {
+            self.tableView.reloadSections([0], with: .automatic)
+        }, errorBlock: { (error) in
+            print("Realm error reloading rows", error)
+        }))
+        rows2NotificationToken = rows2.observe(sectionedNotificationTokenBlock.block(forSection: 1, initialBlock: {
+            self.tableView.reloadSections([1], with: .automatic)
+        }, errorBlock: { (error) in
+            print("Realm error reloading rows", error)
+        }))
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "basicTableViewCell", for: indexPath)
